f20a4a3f512058177b6c9b3e7784b5e92cd753aa
superstar323 <carlosarthur0323@gmail.com>
2022-06-26 23:44:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { useMetaMask } from "metamask-react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
+
+  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+
+  if (status === "unavailable") return <div>MetaMask not available :(</div>
+
+  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+
+  if (status === "connecting") return <div>Connecting...</div>
+
+  if (status === "connected") {
+    if (chainId === '0x1') {
+      return <div>Connected account: {account}</div>
+    } else {
+      alert('network is not ethereum.<br />Reconnect the net');
+      return <button onClick={connect}>Connect to MetaMask</button>
+    }
+    console.log(account, ethereum, chainId, status, 8989)
+  }
+
+  return null;
 }
 
 export default App;
